f7c1c5b22edd2210b82cb8c1ebd89ed49defcd15
Malin <malinlundhe@hotmail.com>
2023-11-02 16:32:07 +0100

    added header and section1

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,1 +1,19 @@
+import { useTranslation } from "react-i18next";
+
+const Section1 = () => {
+    const { t } = useTranslation("Section1");
+
+    return (
+        <section className="section1">
+            <div className="logo-container">
+                <img src="/logo.png" alt="Logo" />
+            </div>
+            <div className="text-container">
+                <h2>{t("title")}</h2>
+            </div>
+        </section>
+    );
+};
+
+
 export default Section1;
